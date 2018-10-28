f844db35c1439f07cf5c6fc4a9cfe29a6f613b1c
Mariusz Wcislo <mariuszwcislo1993@gmail.com>
2018-10-28 13:03:23 +0100

    Add test dependecies

---
+++ src/test/java/service/UserServiceImplTest.java
@@ -0,0 +1,16 @@
+package service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class UserServiceImplTest {
+
+
+    @Test
+    public void getUserByUsername() {
+
+
+    }
+}
